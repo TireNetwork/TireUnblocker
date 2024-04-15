c3243c390659bfbb2223cd2c5209382579d516e4
Wheels <154562751+Wheels522@users.noreply.github.com>
2024-04-14 21:06:24 -0500

    Update uv.js

--- uv.js
+++ uv.js
@@ -1,6 +1,6 @@
-importScripts('/static/uv/uv.bundle.js');
-importScripts('/static/uv/uv.config.js');
-importScripts('/static/uv/uv.sw.js');
+importScripts('uv/uv.bundle.js');
+importScripts('uv/uv.config.js');
+importScripts('uv/uv.sw.js');
 importScripts('https://arc.io/arc-sw-core.js');
 
 const sw = new UVServiceWorker();
