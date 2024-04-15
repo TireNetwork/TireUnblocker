82b5d0de2c100a1d05212a533d98221acee021ee
Wheels <154562751+Wheels522@users.noreply.github.com>
2024-04-14 21:09:56 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare:'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
-    config: '/static/uv/uv.config.js',
-    sw: '/static/uv/uv.sw.js',
+    handler: 'uv/uv.handler.js',
+    bundle: 'uv/uv.bundle.js',
+    config: 'uv/uv.config.js',
+    sw: 'uv/uv.sw.js',
 };
