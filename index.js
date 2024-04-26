8e27ab8de5bee763eeea8b631452fe9b6a494b25
Wheels <154562751+Wheels522@users.noreply.github.com>
2024-04-25 20:12:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const address = document.getElementById("search");
  * @type {HTMLInputElement}
  */
 const searchEngine = document.getElementById("searchengine");
-
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   try {
@@ -20,31 +19,32 @@ form.addEventListener("submit", async (event) => {
     alert(err.toString());
     throw err;
   }
+  const url = search(address.value, searchEngine.value);
+  var white = document.createElement('div');
+  white.style.cursor = "pointer";
+  white.style.position = "absolute";
+  white.style.width = "100%";
+  white.style.height = "100%";
+  white.style.zIndex = "100";
+  white.style.right = "0px";
+  white.className = "black";
+  white.style.top = "0px";
+  document.body.appendChild(white);
+   var iframe = document.createElement('iframe');
+   iframe.style.position = "absolute";
+   iframe.style.width = "100%";
+   iframe.style.height = "100%";
+   iframe.style.top = "0px";
+   iframe.style.left = "0px";
+   iframe.id = "iframe";
+   iframe.style.zIndex = "1000";
+ @@ -47,6 +47,10 @@ form.addEventListener("submit", async (event) => {
 
-  var iframe = document.createElement('iframe');
-  iframe.style.position = "absolute";
-  iframe.style.width = "100%";
-  iframe.style.height = "100%";
-  iframe.style.top = "0px";
-  iframe.style.left = "0px";
-  iframe.id = "iframe";
-  iframe.style.zIndex = "1000";
-  iframe.style.border = "none";
-  iframe.src = __uv$config.prefix + __uv$config.encodeUrl(address.value);
-  document.body.appendChild(iframe);
-
-  var behindIframe = document.createElement('div');
-  behindIframe.style.position = "absolute";
-  behindIframe.style.width = "100%";
-  behindIframe.style.height = "100%";
-  behindIframe.style.top = "0px";
-  behindIframe.style.left = "0px";
-  behindIframe.style.zIndex = "-1";
-  behindIframe.style.backgroundColor = "white"; // Set the background color to white
-  behindIframe.id = "behindIframe";
-  document.body.appendChild(behindIframe);
-});
-
-document.getElementById("iframe").addEventListener("load", function(){
-  document.getElementById("behindIframe").style.display = "block";
+   var button = document.createElement('button');
+   button.textContent = "Get iframe URL and Redirect";
+   button.onclick = function() {
+     var iframeUrl = iframe.src;
+     window.location.href = iframeUrl;
+  };
+  document.body.appendChild(button);
 });
