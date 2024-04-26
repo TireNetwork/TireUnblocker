761ac0342ae104f2ccc17635a40e88ce6b7c69ad
Wheels <154562751+Wheels522@users.noreply.github.com>
2024-04-26 07:07:42 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,71 +2,35 @@
 /**
  * @type {HTMLFormElement}
  */
-const form = document.getElementById("searchbox");
+const form = document.getElementById("uv-form");
 /**
  * @type {HTMLInputElement}
  */
-const address = document.getElementById("search");
+const address = document.getElementById("uv-address");
 /**
  * @type {HTMLInputElement}
  */
-const searchEngine = document.getElementById("searchengine");
+const searchEngine = document.getElementById("uv-search-engine");
+/**
+ * @type {HTMLParagraphElement}
+ */
+const error = document.getElementById("uv-error");
+/**
+ * @type {HTMLPreElement}
+ */
+const errorCode = document.getElementById("uv-error-code");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
+
   try {
     await registerSW();
   } catch (err) {
-    alert(err.toString());
+    error.textContent = "Failed to register service worker.";
+    errorCode.textContent = err.toString();
     throw err;
   }
+
   const url = search(address.value, searchEngine.value);
-
-  var white = document.createElement('div');
-  white.style.cursor = "pointer";
-  white.style.position = "absolute";
-  white.style.width = "100%";
-  white.style.height = "100%";
-  white.style.zIndex = "100";
-  white.style.right = "0px";
-  white.className = "black";
-  white.style.top = "0px";
-  document.body.appendChild(white);
-  var open = document.createElement('div');
-open.style.cursor = "pointer";
-open.style.position = "absolute";
-open.style.width = "50px";
-open.style.height = "50px";
-open.style.zIndex = "1001";
-open.style.right = "65px";
-open.style.top = "1%";
-open.style.backgroundColor = "blue";
-open.style.color = "white";
-open.style.textAlign = "center";
-open.style.lineHeight = "50px";
-open.innerText = "Open";
-
-open.onclick = function() {
-    var iframeUrl = document.getElementById("iframe").getAttribute("src");
-    if (iframeUrl && iframeUrl !== "about:blank"){
-        window.open(iframeUrl);
-    }
-};
-
-document.body.appendChild(open);
-
-  var iframe = document.createElement('iframe');
-  iframe.style.position = "absolute";
-  iframe.style.width = "100%";
-  iframe.style.height = "calc(100% - 40px)";
-  iframe.style.top = "40px"; // Adjusted top position to accommodate the button
-  iframe.style.left = "0px";
-  iframe.id = "iframe";
-  iframe.style.zIndex = "1000";
-  iframe.style.border = "none";
-  iframe.src = __uv$config.prefix + __uv$config.encodeUrl(url);
-  document.body.appendChild(iframe);
-
-  };
-  document.body.appendChild(button);
+  location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
 });
