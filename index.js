f760a03267af8f3df331de21dc68991c57f60c5c
Wheels <154562751+Wheels522@users.noreply.github.com>
2024-04-27 15:53:19 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import http from "node:http";
+import path from "node:path";
+import createBareServer from "@tomphttp/bare-server-node";
+
+const __dirname = process.cwd();
+const server = http.createServer();
+const app = express(server);
+const bareServer = createBareServer("/bare/");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "static")));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/index.html'));
+});
+
+server.on("request", (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    app(req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+server.on("listening", () => {
+  console.log(`Tire Unblocker has successfully started!\nListening on localhost (Port 8000).`);
+});
+
+server.listen({
+  port: 8000,
+});
