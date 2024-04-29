75c27fff7e23caf879723e45e82976423b07f0d4
Wheels <154562751+Wheels522@users.noreply.github.com>
2024-04-29 16:56:14 -0500

    Add files via upload

---
+++ static/uv/uv.config.js
@@ -0,0 +1,13 @@
+self.__uv$config = {
+    prefix: '/service/',
+    bare: {
+    version: 2,
+    path: "/bare/",
+  },
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv/uv.handler.js',
+    bundle: '/uv/uv.bundle.js',
+    config: '/uv/uv.config.js',
+    sw: '/uv/uv.sw.js',
+};
