a3f34da9a7d308cc7491b2ea98363f57fe4bdb15
Wheels <154562751+Wheels522@users.noreply.github.com>
2024-04-29 16:51:01 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+  routes: true, // Change this to false if you just want to host a bare server.
+  local: true, // Change this to false to disable local assets.
+}
+export default config
