2c14dd1924306ec44e47c3a57a5eed92e05c1687
Wheels <154562751+Wheels522@users.noreply.github.com>
2024-04-29 20:38:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 "use strict";
+
+const urlBarHTML = `
+<div id="url-bar" style="width: 100%; text-align: left; background-color: grey; padding: 10px 0; position: fixed; top: 0; left: 0; z-index: 1000;">
+  <form id="daform" style="text-align: left; width: 700px; margin: 50px auto 0;">
+    <input id="daaddress" type="text" placeholder="Search Google or type a URL" class="form__input" style="width: 100%; padding: 10px; margin-top: 10px; text-align: left; border-radius: 18px; outline: none; background: transparent; border: 2px solid white; color: grey; width: 600px; height: 50px;"/>
+    <button id="reload-button" style="cursor: pointer; background-color: grey; color: white; border: 2px solid white; padding: 8px 10px; border-radius: 5px; margin-top: 10px;">Reload</button>
+  </form>
+</div>
+`;
+
+document.body.insertAdjacentHTML('afterbegin', urlBarHTML);
+
 /**
  * @type {HTMLFormElement}
  */
@@ -33,4 +45,9 @@ form.addEventListener("submit", async (event) => {
 
   const url = search(address.value, searchEngine.value);
   location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+});
+
+document.getElementById('reload-button').addEventListener('click', function() {
+  var url = document.getElementById('daaddress').value;
+  window.location.href = url;
 });
