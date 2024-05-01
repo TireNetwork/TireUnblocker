d37e18e0c5c7de382d0209c28fa7e649cc96447a
Wheels <154562751+Wheels522@users.noreply.github.com>
2024-05-01 16:20:42 -0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-  routes: true, // Change this to false if you just want to host a bare server.
-  local: true, // Change this to false to disable local assets.
-}
-export default config
