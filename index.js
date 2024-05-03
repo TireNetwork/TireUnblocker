9488f3365446e82c0f4db6eccc385cb8963cee37
Wheels <154562751+Wheels522@users.noreply.github.com>
2024-05-03 07:04:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 8080
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'static')))
+app.use(express.static(path.join(__dirname, '')))
 
 if (config.routes !== false) {
   const routes = [
