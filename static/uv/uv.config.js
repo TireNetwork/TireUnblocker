263c8f2b58583ea5f860e4bdbd71304fadc22bc3
Wheels <154562751+Wheels522@users.noreply.github.com>
2024-05-10 18:29:30 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,8 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: {
-    version: 2,
-    path: "/bare/",
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
